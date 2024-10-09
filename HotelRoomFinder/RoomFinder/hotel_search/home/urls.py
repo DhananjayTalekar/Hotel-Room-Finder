30be77819d28c6543f6fa655d2248d64434ef085
Dhananjay Talekar <162672004+DhananjayTalekar@users.noreply.github.com>
2024-10-10 00:56:42 +0530

    Delete HotelRoomFinder/RoomFinder directory

--- HotelRoomFinder/RoomFinder/hotel_search/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, get_hotel
-
-urlpatterns = [
-    path('', home),  # This should render the home page
-    path('api/get_GFG/', get_hotel),  # API endpoint for hotel data
-]
-
